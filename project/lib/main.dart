f62f253810b39dfcaee3a594d390a3c146abc48d
JIK-K <87060676+JIK-K@users.noreply.github.com>
2023-01-28 21:58:53 +0900

    main layout create

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -12,8 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: '금연노트',
         theme: ThemeData(
-          fontFamily: 'SCDreams',
-        ),
+            fontFamily: 'SCDreams', scaffoldBackgroundColor: Color(0xffFFFBE9)),
         home: Scaffold(
           //Top
           appBar: AppBar(
@@ -22,114 +21,211 @@ class MyApp extends StatelessWidget {
             title: Text(
               '금연노트',
               style: TextStyle(
-                color: Colors.amber,
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'assets/fonts/SCDreams.otf'),
             ),
-            backgroundColor: Color(0xffE6DDC4),
+            backgroundColor: Color(0xffAD8B73),
           ),
 
           //Body
           body: Column(
-            children: [
+            children: <Widget>[
+              //Picture Area
               Flexible(
+                  fit: FlexFit.tight,
                   flex: 4,
                   child: Container(
                     padding: EdgeInsets.all(5),
                     decoration: BoxDecoration(
-                        border: Border.all(
-                      width: 5,
-                      color: Colors.black,
-                    )),
+                        border: Border(
+                            left:
+                                BorderSide(width: 6, color: Color(0xffE3CAA5)),
+                            top: BorderSide(width: 6, color: Color(0xffE3CAA5)),
+                            right: BorderSide(
+                                width: 6, color: Color(0xffE3CAA5)))),
+                    child: Container(
+                      child:
+                          Image.asset('assets/happyday.png', fit: BoxFit.fill),
+                    ),
                   )),
-              Flexible(
-                  flex: 3,
-                  child: Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      width: 5,
-                      color: Colors.yellow,
-                    )),
-                  )),
-              Flexible(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    width: 5,
-                    color: Colors.blue,
-                  )),
-                ),
-              ),
-              Flexible(
-                  flex: 1,
-                  child: Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      width: 5,
-                      color: Colors.redAccent,
-                    )),
-                  )),
+
+              //Smoking Time Area
               Flexible(
                   flex: 2,
                   child: Container(
                     padding: EdgeInsets.all(5),
                     decoration: BoxDecoration(
-                        border: Border.all(
-                      width: 5,
-                      color: Colors.teal,
+                        border: Border(
+                      left: BorderSide(width: 6, color: Color(0xffE3CAA5)),
+                      right: BorderSide(width: 6, color: Color(0xffE3CAA5)),
+                      top: BorderSide(width: 6, color: Color(0xffE3CAA5)),
                     )),
                   )),
+              //Save Time & Money Area
+              Flexible(
+                flex: 1,
+                child: Row(
+                  children: [
+                    //Save Money Area
+                    Flexible(
+                        child: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                border: Border(
+                              left: BorderSide(
+                                  width: 6, color: Color(0xffE3CAA5)),
+                              right: BorderSide(
+                                  width: 3, color: Color(0xffE3CAA5)),
+                              top: BorderSide(
+                                  width: 6, color: Color(0xffE3CAA5)),
+                            )),
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [Text('절약한 금액'), Text('1000₩')],
+                              ),
+                            ))),
+                    //Spent Money Area
+                    Flexible(
+                        child: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                border: Border(
+                              left: BorderSide(
+                                  width: 3, color: Color(0xffE3CAA5)),
+                              right: BorderSide(
+                                  width: 6, color: Color(0xffE3CAA5)),
+                              top: BorderSide(
+                                  width: 6, color: Color(0xffE3CAA5)),
+                            )),
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [Text('소비한 금액'), Text('112121₩')],
+                              ),
+                            )))
+                  ],
+                ),
+              ),
+              Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      //Save Time Area
+                      Flexible(
+                          child: Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            border: Border(
+                          left: BorderSide(width: 6, color: Color(0xffE3CAA5)),
+                          right: BorderSide(width: 3, color: Color(0xffE3CAA5)),
+                          top: BorderSide(width: 6, color: Color(0xffE3CAA5)),
+                        )),
+                        child: Center(
+                            child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [Text('절약한 시간'), Text('12시간15분')],
+                        )),
+                      )),
+                      //Spent Time Area
+                      Flexible(
+                          child: Container(
+                              padding: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                left: BorderSide(
+                                    width: 3, color: Color(0xffE3CAA5)),
+                                right: BorderSide(
+                                    width: 6, color: Color(0xffE3CAA5)),
+                                top: BorderSide(
+                                    width: 6, color: Color(0xffE3CAA5)),
+                              )),
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [Text('낭비된 시간'), Text('123시간')],
+                                ),
+                              )))
+                    ],
+                  )),
+              //Icon Button Area
               Flexible(
                   flex: 1,
                   child: Container(
                     padding: EdgeInsets.all(5),
                     decoration: BoxDecoration(
-                        border: Border.all(
-                      width: 5,
-                      color: Colors.lightGreen,
+                        border: Border(
+                      left: BorderSide(width: 6, color: Color(0xffE3CAA5)),
+                      right: BorderSide(width: 6, color: Color(0xffE3CAA5)),
+                      top: BorderSide(width: 6, color: Color(0xffE3CAA5)),
                     )),
-                    child: Row(
-                      children: [
-                        Flexible(
-                            flex: 1,
-                            child: Container(
-                              padding: EdgeInsets.all(5),
-                              margin: EdgeInsets.all(2),
-                              decoration: BoxDecoration(
-                                  border: Border.all(
-                                width: 3,
-                                color: Colors.lightGreen,
-                              )),
+                  )),
+              //Counting Area
+              Flexible(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Flexible(
+                          fit: FlexFit.tight,
+                          flex: 1,
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                border: Border(
+                              top: BorderSide(
+                                  width: 6, color: Color(0xffE3CAA5)),
+                              left: BorderSide(
+                                  width: 6, color: Color(0xffE3CAA5)),
+                              right: BorderSide(
+                                  width: 3, color: Color(0xffE3CAA5)),
+                              bottom: BorderSide(
+                                  width: 6, color: Color(0xffE3CAA5)),
                             )),
-                        Flexible(
-                            flex: 1,
-                            child: Container(
-                              padding: EdgeInsets.all(5),
-                              margin: EdgeInsets.all(2),
-                              decoration: BoxDecoration(
-                                  border: Border.all(
-                                width: 3,
-                                color: Colors.lightGreen,
-                              )),
+                            child: Center(child: Text('test1')),
+                          )),
+                      Flexible(
+                          fit: FlexFit.tight,
+                          flex: 1,
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                border: Border(
+                              top: BorderSide(
+                                  width: 6, color: Color(0xffE3CAA5)),
+                              left: BorderSide(
+                                  width: 3, color: Color(0xffE3CAA5)),
+                              right: BorderSide(
+                                  width: 3, color: Color(0xffE3CAA5)),
+                              bottom: BorderSide(
+                                  width: 6, color: Color(0xffE3CAA5)),
                             )),
-                        Flexible(
-                            flex: 1,
-                            child: Container(
-                              padding: EdgeInsets.all(5),
-                              margin: EdgeInsets.all(2),
-                              decoration: BoxDecoration(
-                                  border: Border.all(
-                                width: 3,
-                                color: Colors.lightGreen,
-                              )),
-                            ))
-                      ],
-                    ),
+                            child: Center(child: Text('test2')),
+                          )),
+                      Flexible(
+                          fit: FlexFit.tight,
+                          flex: 1,
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                border: Border(
+                              top: BorderSide(
+                                  width: 6, color: Color(0xffE3CAA5)),
+                              left: BorderSide(
+                                  width: 3, color: Color(0xffE3CAA5)),
+                              right: BorderSide(
+                                  width: 6, color: Color(0xffE3CAA5)),
+                              bottom: BorderSide(
+                                  width: 6, color: Color(0xffE3CAA5)),
+                            )),
+                            child: Center(child: Text('test3')),
+                          )),
+                    ],
                   )),
             ],
           ),
