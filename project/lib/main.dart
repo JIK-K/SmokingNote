3f2fa17affb70c2907ec2c568a805377d518133b
JIK-K <87060676+JIK-K@users.noreply.github.com>
2023-01-30 17:51:14 +0900

    main layout & widget finish

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: '금연노트',
         theme: ThemeData(
-            fontFamily: 'SCDreams', scaffoldBackgroundColor: Color(0xffFFFBE9)),
+            fontFamily: 'SCDream5',
+            scaffoldBackgroundColor: Colors.white /*Color(0xffFFFBE9)*/),
         home: Scaffold(
           //Top
           appBar: AppBar(
@@ -22,9 +23,8 @@ class MyApp extends StatelessWidget {
               '금연노트',
               style: TextStyle(
                   color: Colors.white,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'assets/fonts/SCDreams.otf'),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
             ),
             backgroundColor: Color(0xffAD8B73),
           ),
@@ -39,30 +39,57 @@ class MyApp extends StatelessWidget {
                   child: Container(
                     padding: EdgeInsets.all(5),
                     decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/test12.png'),
+                            fit: BoxFit.fill),
                         border: Border(
                             left:
-                                BorderSide(width: 6, color: Color(0xffE3CAA5)),
-                            top: BorderSide(width: 6, color: Color(0xffE3CAA5)),
+                                BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                            top: BorderSide(width: 4, color: Color(0xffE3CAA5)),
                             right: BorderSide(
-                                width: 6, color: Color(0xffE3CAA5)))),
-                    child: Container(
-                      child:
-                          Image.asset('assets/happyday.png', fit: BoxFit.fill),
-                    ),
+                                width: 4, color: Color(0xffE3CAA5)))),
                   )),
 
               //Smoking Time Area
               Flexible(
-                  flex: 2,
-                  child: Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        border: Border(
-                      left: BorderSide(width: 6, color: Color(0xffE3CAA5)),
-                      right: BorderSide(width: 6, color: Color(0xffE3CAA5)),
-                      top: BorderSide(width: 6, color: Color(0xffE3CAA5)),
-                    )),
+                flex: 3,
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      border: Border(
+                    left: BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                    right: BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                    top: BorderSide(width: 4, color: Color(0xffE3CAA5)),
                   )),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          '금연 시간',
+                          style: TextStyle(
+                              fontSize: 13, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'years      Days      Hours      Mins      Secs',
+                          style: TextStyle(fontSize: 10),
+                        ),
+                        Text(
+                          '00 : 000 : 00 : 00 : 00',
+                          style: TextStyle(fontSize: 30),
+                        ),
+                        Text(
+                          '이제 위에거를 받아서 몇분마다 상태변화가된다는걸적어준다',
+                          style: TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.w800,
+                              color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
               //Save Time & Money Area
               Flexible(
                 flex: 1,
@@ -75,11 +102,11 @@ class MyApp extends StatelessWidget {
                             decoration: BoxDecoration(
                                 border: Border(
                               left: BorderSide(
-                                  width: 6, color: Color(0xffE3CAA5)),
+                                  width: 4, color: Color(0xffE3CAA5)),
                               right: BorderSide(
-                                  width: 3, color: Color(0xffE3CAA5)),
+                                  width: 2, color: Color(0xffE3CAA5)),
                               top: BorderSide(
-                                  width: 6, color: Color(0xffE3CAA5)),
+                                  width: 4, color: Color(0xffE3CAA5)),
                             )),
                             child: Center(
                               child: Column(
@@ -95,11 +122,11 @@ class MyApp extends StatelessWidget {
                             decoration: BoxDecoration(
                                 border: Border(
                               left: BorderSide(
-                                  width: 3, color: Color(0xffE3CAA5)),
+                                  width: 2, color: Color(0xffE3CAA5)),
                               right: BorderSide(
-                                  width: 6, color: Color(0xffE3CAA5)),
+                                  width: 4, color: Color(0xffE3CAA5)),
                               top: BorderSide(
-                                  width: 6, color: Color(0xffE3CAA5)),
+                                  width: 4, color: Color(0xffE3CAA5)),
                             )),
                             child: Center(
                               child: Column(
@@ -121,9 +148,9 @@ class MyApp extends StatelessWidget {
                         padding: EdgeInsets.all(5),
                         decoration: BoxDecoration(
                             border: Border(
-                          left: BorderSide(width: 6, color: Color(0xffE3CAA5)),
-                          right: BorderSide(width: 3, color: Color(0xffE3CAA5)),
-                          top: BorderSide(width: 6, color: Color(0xffE3CAA5)),
+                          left: BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                          right: BorderSide(width: 2, color: Color(0xffE3CAA5)),
+                          top: BorderSide(width: 4, color: Color(0xffE3CAA5)),
                         )),
                         child: Center(
                             child: Column(
@@ -138,11 +165,11 @@ class MyApp extends StatelessWidget {
                               decoration: BoxDecoration(
                                   border: Border(
                                 left: BorderSide(
-                                    width: 3, color: Color(0xffE3CAA5)),
+                                    width: 2, color: Color(0xffE3CAA5)),
                                 right: BorderSide(
-                                    width: 6, color: Color(0xffE3CAA5)),
+                                    width: 4, color: Color(0xffE3CAA5)),
                                 top: BorderSide(
-                                    width: 6, color: Color(0xffE3CAA5)),
+                                    width: 4, color: Color(0xffE3CAA5)),
                               )),
                               child: Center(
                                 child: Column(
@@ -155,78 +182,114 @@ class MyApp extends StatelessWidget {
                   )),
               //Icon Button Area
               Flexible(
-                  flex: 1,
+                  flex: 2,
                   child: Container(
                     padding: EdgeInsets.all(5),
                     decoration: BoxDecoration(
                         border: Border(
-                      left: BorderSide(width: 6, color: Color(0xffE3CAA5)),
-                      right: BorderSide(width: 6, color: Color(0xffE3CAA5)),
-                      top: BorderSide(width: 6, color: Color(0xffE3CAA5)),
+                      left: BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                      right: BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                      top: BorderSide(width: 4, color: Color(0xffE3CAA5)),
                     )),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundColor: Colors.indigo,
+                          child: IconButton(
+                            iconSize: 50,
+                            icon:
+                                Icon(Icons.fitness_center, color: Colors.white),
+                            padding: EdgeInsets.zero,
+                            constraints: BoxConstraints(),
+                            onPressed: () {
+                              build(context);
+                            },
+                          ),
+                        ),
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundColor: Colors.orangeAccent,
+                          child: IconButton(
+                            iconSize: 50,
+                            icon: Icon(Icons.sports_bar, color: Colors.white),
+                            padding: EdgeInsets.zero,
+                            constraints: BoxConstraints(),
+                            onPressed: () {
+                              build(context);
+                            },
+                          ),
+                        ),
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundColor: Colors.redAccent,
+                          child: IconButton(
+                            iconSize: 50,
+                            icon:
+                                Icon(Icons.smoking_rooms, color: Colors.white),
+                            padding: EdgeInsets.zero,
+                            constraints: BoxConstraints(),
+                            onPressed: () {
+                              build(context);
+                            },
+                          ),
+                        )
+                      ],
+                    ),
                   )),
               //Counting Area
               Flexible(
-                  flex: 1,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Flexible(
-                          fit: FlexFit.tight,
-                          flex: 1,
-                          child: Container(
-                            padding: EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                border: Border(
-                              top: BorderSide(
-                                  width: 6, color: Color(0xffE3CAA5)),
-                              left: BorderSide(
-                                  width: 6, color: Color(0xffE3CAA5)),
-                              right: BorderSide(
-                                  width: 3, color: Color(0xffE3CAA5)),
-                              bottom: BorderSide(
-                                  width: 6, color: Color(0xffE3CAA5)),
-                            )),
-                            child: Center(child: Text('test1')),
-                          )),
-                      Flexible(
-                          fit: FlexFit.tight,
-                          flex: 1,
-                          child: Container(
-                            padding: EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                border: Border(
-                              top: BorderSide(
-                                  width: 6, color: Color(0xffE3CAA5)),
-                              left: BorderSide(
-                                  width: 3, color: Color(0xffE3CAA5)),
-                              right: BorderSide(
-                                  width: 3, color: Color(0xffE3CAA5)),
-                              bottom: BorderSide(
-                                  width: 6, color: Color(0xffE3CAA5)),
-                            )),
-                            child: Center(child: Text('test2')),
-                          )),
-                      Flexible(
-                          fit: FlexFit.tight,
-                          flex: 1,
-                          child: Container(
-                            padding: EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                                border: Border(
-                              top: BorderSide(
-                                  width: 6, color: Color(0xffE3CAA5)),
-                              left: BorderSide(
-                                  width: 3, color: Color(0xffE3CAA5)),
-                              right: BorderSide(
-                                  width: 6, color: Color(0xffE3CAA5)),
-                              bottom: BorderSide(
-                                  width: 6, color: Color(0xffE3CAA5)),
-                            )),
-                            child: Center(child: Text('test3')),
-                          )),
-                    ],
-                  )),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                      fit: FlexFit.tight,
+                      flex: 1,
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            border: Border(
+                          top: BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                          left: BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                          right: BorderSide(width: 2, color: Color(0xffE3CAA5)),
+                          bottom:
+                              BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                        )),
+                        child: Center(child: Text('운동몇분누를때마다30분')),
+                      )),
+                  Flexible(
+                      fit: FlexFit.tight,
+                      flex: 1,
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            border: Border(
+                          top: BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                          left: BorderSide(width: 2, color: Color(0xffE3CAA5)),
+                          right: BorderSide(width: 2, color: Color(0xffE3CAA5)),
+                          bottom:
+                              BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                        )),
+                        child: Center(child: Text('술몇잔누를때마다한잔')),
+                      )),
+                  Flexible(
+                      fit: FlexFit.tight,
+                      flex: 1,
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            border: Border(
+                          top: BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                          left: BorderSide(width: 2, color: Color(0xffE3CAA5)),
+                          right: BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                          bottom:
+                              BorderSide(width: 4, color: Color(0xffE3CAA5)),
+                        )),
+                        child: Center(child: Text('담배몇개비필때마다한개비')),
+                      )),
+                ],
+              )),
             ],
           ),
 
